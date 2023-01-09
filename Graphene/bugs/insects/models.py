784f0ff4fafb37bf951feeb281276ff3c7bb8e4d
mgurnett <mgurnett@gmail.com>
2023-01-09 15:58:51 -0700

    getting ready to add the models form Graphene test

--- Graphene/bugs/insects/models.py
+++ Graphene/bugs/insects/models.py
@@ -68,3 +68,19 @@ class Strength(models.Model):
 
     def __str__ (self):
         return f'bug: {self.hatch.bug.name} in week: {self.week.number} has a strength of: {self.strength}'
+
+    '''
+    ON DELETE CASCADE: if a row of the referenced table is deleted, then all matching rows 
+    in the referencing table are deleted.
+    ON DELETE SET NULL: if a row of the referenced table is deleted, then all referencing columns 
+    in all matching rows of the referencing table to be set to null.
+    on_delete=models.SET_NULL, null=True)
+    ON DELETE SET DEFAULT: if a row of the referenced table is deleted, then all referencing 
+    columns in all matching rows of the referencing table to be set to the column’s default value.
+    on_delete=models.SET_DEFAULT, default=1)
+    ON DELETE RESTRICT: it is prohibited to delete a row of the referenced table if that row 
+    has any matching rows in the referencing table.
+    ON DELETE NO ACTION (the default): there is no referential delete action; the referential 
+    constraint only specifies a constraint check.
+    NO DELETE PROTECT
+    '''
