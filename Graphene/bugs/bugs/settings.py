7fd620d1c22c2080a1af5b20e4f5da6e9a887df0
mgurnett <mgurnett@gmail.com>
2023-01-09 15:59:04 -0700

    final step

--- Graphene/bugs/bugs/settings.py
+++ Graphene/bugs/bugs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-16%kx!xw)cjz_usmr5_y59u@ppuu*@@*&6+21$#pi%ct7afav+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
