a40aab26718cc25fa730defac2a399f1a63f450a
mgurnett <mgurnett@gmail.com>
2023-01-31 07:48:40 -0700

    going to add url link to lakes for gps

--- catches/views.py
+++ catches/views.py
@@ -272,6 +272,7 @@ class LakeDetailView (DetailView):
         data = Lake.objects.filter (id=self.kwargs['pk']).values_list('static_tag', flat=True)[0]
         context ['videos_list'] = Video.objects.filter (tags__name__contains=data)
         context ['articles_list'] = Article.objects.filter (tags__name__contains=data)
+        context ['pictures_list'] = Picture.objects.filter (tags__name__contains=data)
         context ['pictures_list_bath'] = Picture.objects.filter (tags__name__contains=data) & Picture.objects.filter (tags__name__contains='bathymetric')
         return context
 
