aea0d9fd0be849125ad87ba6b18fb0e19e3f6002
mgurnett <mgurnett@gmail.com>
2023-03-04 15:42:57 -0700

    fixed login redirect

--- FishingLog/settings.py
+++ FishingLog/settings.py
@@ -133,7 +133,7 @@ MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_REDIRECT_URL = 'catch_home'
 LOGIN_URL = 'login'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
