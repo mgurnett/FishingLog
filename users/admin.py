c43374b0c0f0a57e0f6b76f3c8ecb02450c071a2
mgurnett <mgurnett@gmail.com>
2023-03-04 15:18:25 -0700

    added some users

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import Profile
 
 admin.site.register(Profile)
+
+class MyUserAdmin(admin.ModelAdmin):
+    def group(self, user):
+        groups = []
+        for group in user.groups.all():
+            groups.append(group.name)
+        return ' '.join(groups)
+    group.short_description = 'Groups'
+    list_display = ['username', 'first_name', 'last_name', 'is_active', 'group', 'last_login', 'email', ]
+    # list_filter = ("first_name",  )
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
