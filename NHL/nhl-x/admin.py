c4c228519d64909feef0ffdeb33b13c224f99f5c
mgurnett <mgurnett@gmail.com>
2023-04-11 12:39:01 -0600

    I removed NHL

--- NHL/nhl-x/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register (Conference)
-admin.site.register (Division)
-admin.site.register (Team)
-admin.site.register (Player)
--- NHL/nhl-x/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.Home.as_view(), name='home'),
-    path('team/<int:pk>/', views.Team.as_view(), name='team'),
-]
