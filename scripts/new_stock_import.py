a8e826769229681eeaa4a8dd29d0afa494fcc3f9
mgurnett <mgurnett@gmail.com>
2023-05-23 16:23:13 -0600

    getting closer to finalizing stock input

--- scripts/new_stock_import.py
+++ scripts/new_stock_import.py
@@ -23,17 +23,29 @@ STRAIN_lookup = [
     ('Job Lake', 'JBL')
  ]
 
-def get_data():
-    with open('static/stock_reports/2020_raw.txt') as file:
+def get_data(file_name, year):
+    stock_num = 0
+    with open(file_name) as file:
         file_contents = file.read()
+    
+    if year == 0:
+        pattern = '(\d{1,2})[-][A-Z][a-z][a-z]-20'
+    if year == 1:
+        pattern = '(\d{1,2})[/][A-Z][a-z][a-z]/21'
+    if year == 2:
+        pattern = '(\d{1,2})[-][A-Z][a-z][a-z]-22'
+    if year == 3:
+        pattern = '(\d{1,2})[-][A-Z][a-z][a-z]-23'
 
     lines = []
     new_end = 0
-    for x in re.finditer ('(\d{1,2})[-][A-Z][a-z][a-z]-20', file_contents):  #find where the line ends as the date is aways the end
+    for x in re.finditer (pattern, file_contents):  #find where the line ends as the date is aways the end
         start, end = x.span()
         date_obj = x.group()
-        # stock_date = datetime.strptime(date_obj, "%d/%b/%y") #for 2021
-        stock_date = datetime.strptime(date_obj, "%d-%b-%y") #for 2022 & 3
+        if year == 1:
+            stock_date = datetime.strptime(date_obj, "%d/%b/%y") #for 2021
+        else:
+            stock_date = datetime.strptime(date_obj, "%d-%b-%y") #for 2022 & 3
 
         row = str(file_contents[new_end:end]) #get the whole row
         # print (row)
@@ -85,7 +97,7 @@ def get_data():
 
         length_string = str(row[geno_end:])
         
-        fish_str = re.search ("\d*\.?\d", length_string)
+        fish_str = re.search ("\d*\.?\d+", length_string)
         # print (fish_length, " ", row, " ", length_string)
         fish_len = fish_str.group()
         fish_length = float(fish_len)
@@ -100,8 +112,35 @@ def get_data():
         new_end = end + 1 #get set to read the next row
 
         lines.append ([row, stock_date, lake_id, fish_id, genotype, strain, fish_length, number_of_fish])
+        stock_num += 1
+    print (stock_num)
     return lines
 
 def run():
-    data = get_data()
-    # print (data)
+    data2020 = get_data('static/stock_reports/2020_raw.txt', 0)
+    print ("2020 is good")
+    data2021 = get_data('static/stock_reports/2021_raw.txt', 1)
+    print ("2021 is good")
+    data2022 = get_data('static/stock_reports/2022_raw.txt', 2)
+    print ("2022 is good")
+    data2023 = get_data('static/stock_reports/2023_raw.txt', 3)
+    print ("2023 is good")
+
+    total_data = data2020 + data2021 + data2022 + data2023
+
+    # print (total_data)
+
+    with open(r'stock_inputs.txt', 'w') as fp:
+
+        for row in total_data:
+            stock = Stock (
+                date_stocked = row[1],
+                number = row[7],
+                length = row[6],
+                lake = row[2],
+                fish = row[3],
+                strain = row[5],
+                gentotype = row[4],
+            )
+            # print (stock)
+            fp.write("%s\n" % stock)
