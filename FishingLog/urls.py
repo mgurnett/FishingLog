62b0f28defd7ff1d7471c8c764f5efe9e7c16e5a
mgurnett <mgurnett@gmail.com>
2024-04-12 13:15:09 -0600

    getting ready to change picture locations

--- FishingLog/urls.py
+++ FishingLog/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
+from django.urls import re_path
+from django.views.static import serve
  
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,9 +21,9 @@ urlpatterns = [
     path ('blog/', include ('blog.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-            static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+#             static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     
 admin.site.site_header = 'FishingLog administration'              # default: "Django Administration"
 admin.site.index_title = 'Welcome to the FishingLog admin area'   # default: "Site administration"  browser title
