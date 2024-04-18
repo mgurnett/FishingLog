cd688978a407195e5f8eff311246868c065c327f
mgurnett <mgurnett@gmail.com>
2024-04-18 11:42:13 -0600

    update urls

--- FishingLog/urls.py
+++ FishingLog/urls.py
@@ -21,13 +21,10 @@ urlpatterns = [
     path ('blog/', include ('blog.urls')),
 ]
 
-# DEBUG = True
-if settings.DEBUG:
+from django.conf.urls.static import static
 
-    from django.conf.urls.static import static
-
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
 admin.site.site_header = 'FishingLog administration'              # default: "Django Administration"
 admin.site.index_title = 'Welcome to the FishingLog admin area'   # default: "Site administration"  browser title
