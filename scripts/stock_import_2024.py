713d270a45acf8843f34b5136a7776a190a93089
mgurnett <mgurnett@gmail.com>
2024-05-10 16:16:22 -0600

    updated stocks

--- scripts/stock_import_2024.py
+++ scripts/stock_import_2024.py
@@ -3,6 +3,8 @@ import re
 from datetime import datetime
 import csv
 
+# https://www.adobe.com/acrobat/online/pdf-to-excel.html
+
 STRAIN_lookup = [
     ('Beitty x Beitty', 'BEBE'),
     ('Beitty Resort', 'BEBE'),
@@ -40,8 +42,11 @@ STRAIN = (
     ("RD", 'Riverence'),
 )
 
+
 def run():
-    with open('extra files/stockreport as of April 27.csv') as file:
+    Stock.objects.filter(date_stocked__year=2024).delete()
+
+    with open('extra files/stockreport as of May 4.csv') as file:
         reader = csv.reader(file)
         next(reader)  # Advance past the header
 
@@ -76,7 +81,7 @@ def run():
                 geo = ""
 
             stock = Stock (
-                date_stocked = row[8],
+                date_stocked = str(row[8]),
                 number = row[7],
                 length = row[6],
                 lake = lake_id,
@@ -85,7 +90,7 @@ def run():
                 gentotype = geo,
                 )
             total_fish_stocked += int(row[7])
-            # print (f'{stock_count+2} - {stock}')
-            # stock.save()
-            # print (f'{stock_count+2} - {stock}')
+            print (f'{stock_count+2} - {stock}')
+            stock.save()
+            print (f'{stock_count+2} - {stock}')
         print (f'total number of fish stocked is: {total_fish_stocked}')
