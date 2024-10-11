7e74ab1e27e5a6286ef4e444a18e72614f1ffca0
mgurnett <mgurnett@gmail.com>
2024-10-11 15:46:01 -0600

    more picture work

--- scripts/pictures.py
+++ scripts/pictures.py
@@ -1,6 +1,6 @@
 from catches.models import *
 from catches.views import *
-from PIL import Image, ImageOps, ExifTags
+from PIL import Image, ImageOps, ExifTags, ImageFilter
 from PIL.ExifTags import TAGS
 
 def reorient_img(pil_img):
@@ -16,19 +16,80 @@ def reorient_img(pil_img):
 
     return pil_img
 
-def print_exif_data(exif_data):
-    for tag_id in exif_data:
-        tag = TAGS.get(tag_id, tag_id)
-        content = exif_data.get(tag_id)
-        print(f'{tag:25}: {content}')
+def print_exif_data(exifdata):
+    for tag_id in exifdata:
+        tag = TAGS.get(tag_id,tag_id)
+        print(tag_id)
+        data = exifdata.get(tag_id)
+        # we decode bytes
+        if isinstance(data,bytes):
+            data = data.decode()
+        print(f'{tag:25}: {data}')
+
+def resize_picture (image, new_width):
+    #Resize by maintaining aspect ratio (e.g., specify the width)
+    aspect_ratio_preserved = image.resize((new_width, int(image.height * (new_width / image.width))))
+    return aspect_ratio_preserved
+
+def crop_pict (image, left, upper, right, lower ):
+    #Define the coordinates for the region to be cropped (left, upper, right, lower)
+ 
+
+    #Crop the image using the coordinates
+    cropped_image = image.crop((left, upper, right, lower))
+    return cropped_image
 
 def run():
-    image = Image.open ("public/uploads/pictures/image_kNgIFO3.jpg")
-    exif = image.getexif()
+    images = [
+        "blog_pict.png",
+        "fly_rods.jpeg",
+        "Mitchel.jpeg", #(image, 0, 44, 225, 212)
+        "tiger-trout.jpeg",  #h_crop = 40 & 300
+        "dragon_nymph.jpeg",
+        "Parachute.png",
+    ]
+    # for im_name in images:
+    #     image = Image.open ("public/assets/catches/site/back_up/" + im_name)
+    #     image_mod = resize_picture (image)
+    #     image_mod.save ("public/assets/catches/site/" + im_name)
+    #     image_mod.close()
+
+    im_name = "dragon_nymph.jpeg"
+    image = Image.open ("public/assets/catches/site/back_up/" + im_name)
+    image.save ("public/assets/catches/site/" + im_name)
+    h_crop = 5
+    image_mod = crop_pict (image, 0, h_crop, 252, h_crop+189)  #(left, upper, right, lower)
+    image_mod.save ("public/assets/catches/site/crop_" + im_name)
+    image_mod = image_mod.filter(ImageFilter.SHARPEN)
+    image_mod.save ("public/assets/catches/site/sharpen_" + im_name)
+    # image_mod = resize_picture (image_mod, 100)
+    # image_mod.save ("public/assets/catches/site/resize_" + im_name)
+
+    # image_mod.show()
+    image_mod.close()
+'''
+    im_name = "Mitchel.jpeg"
+    image = Image.open ("public/assets/catches/site/back_up/" + im_name)
+    # image.save ("public/assets/catches/site/" + im_name)
+    image_mod = crop_pict (image, 0, 44, 225, 212)  #(left, upper, right, lower)
+    # image_mod.save ("public/assets/catches/site/crop_" + im_name)
+    image_mod = image_mod.filter(ImageFilter.SHARPEN)
+    # image_mod.save ("public/assets/catches/site/sharpen_" + im_name)
+    image_mod = resize_picture (image_mod, 100)
+    image_mod.save ("public/assets/catches/site/resize_" + im_name)
+
+    # image_mod.show()
+    image_mod.close()
+'''
+
+
+
+
+    # exif = image.getexif()
     
-    print_exif_data(exif)
-    print()
-    print_exif_data(exif.get_ifd(0x8769))
+    # print_exif_data(exif)
+    # print()
+    # print_exif_data(exif.get_ifd(0x8769))
     # image.show()
     # img_exif = image.getexif()
     # print (f'{img_exif = }')
@@ -37,4 +98,6 @@ def run():
     # image_mod.show()
     # image_mod.save ("public/uploads/pictures/mods/image_kNgIFO3.jpg")
     # image_mod.close()
+
+
 
