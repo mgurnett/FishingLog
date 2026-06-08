d87706679cfe999a804fcf2da6d03a82cb09d3bf
Michael Gurnett <mgurnett@gmail.com>
2026-06-07 21:57:15 -0600

    Added density

--- catches/templatetags/catches_tags.py
+++ catches/templatetags/catches_tags.py
@@ -7,3 +7,12 @@ register = template.Library()
 def week_list():
     weeks = Week.objects.all()
     return {'weeks': weeks}
+
+@register.simple_tag
+def density_display(area, stock_num):
+    try:
+        # Convert both to float to handle decimals and prevent integer division issues
+        return f"{(float(stock_num) / float(area) /100):.0f} fish/ha"
+    except (ValueError, ZeroDivisionError, TypeError):
+        # Return an empty string or 0 if there's a math/type error (e.g., division by zero)
+        return ''
