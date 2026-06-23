2a3889d31297a2fab5d85978723beabad7630636
Michael Gurnett <mgurnett@gmail.com>
2026-06-23 12:22:44 -0600

    Did a lot of work on the weather page.  Added things like preasure trends and icons.

--- catches/helpers/Open_Weather.py
+++ catches/helpers/Open_Weather.py
@@ -18,13 +18,28 @@ def make_url (lat, lon):
     return f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&units=metric&appid={OW_API_KEY}'
 
 def get_data (lake):
-    # print (make_url (float(round(lake.lat,4)), float(round(lake.long,4))) )
     return requests.get( make_url (float(round(lake.lat,4)), float(round(lake.long,4))) )
-    # You can download all ECCC weather icons from the following URL: https://meteo.gc.ca/weathericons/NN.gif, where NN is a number between 00 and 48.
-    #https://api.openweathermap.org/data/3.0/onecall?lat=53.6247&lon=-113.9564&units=metric&appid=[api key]
 
 def time_convert (time, offset):
     return datetime.fromtimestamp( time + offset )
+    
+def get_moon_phase_info(val):
+    if val == 0 or val == 1:
+        return "New Moon", "bi-moon"
+    elif 0 < val < 0.25:
+        return "Waxing Crescent", "bi-moon-stars"
+    elif val == 0.25:
+        return "First Quarter", "bi-moon-text"
+    elif 0.25 < val < 0.5:
+        return "Waxing Gibbous", "bi-moon-stars"
+    elif val == 0.5:
+        return "Full Moon", "bi-moon-fill"
+    elif 0.5 < val < 0.75:
+        return "Waning Gibbous", "bi-moon-stars"
+    elif val == 0.75:
+        return "Last Quarter", "bi-moon"
+    else:
+        return "Waning Crescent", "bi-moon-stars"
 
 def current (response):
     current = {}
@@ -43,6 +58,7 @@ def current (response):
         current['humidity'] =           data['current']['humidity']
         current['clouds'] =             data['current']['clouds']
         current['description'] =        data['current']['weather'][0]['description']
+        current['icon'] =               data['current']['weather'][0]['icon']  # <-- Extracted current icon code
         current['wind_speed'] =         float(round(data['current']['wind_speed'],1))
         current['wind_deg'] =           data['current']['wind_deg']
         current['wind_direction'] =     degToCompass (data['current']['wind_deg'])
@@ -51,6 +67,7 @@ def current (response):
         except:
             current['wind_gust'] = 0
     return current
+
 def temp_graph (response):
     pofp = []
 
@@ -62,14 +79,11 @@ def temp_graph (response):
         for minute in data.get('minutely', []):
             index += 1
             percent += minute['precipitation']*100
-            # print (f'{percent = }')
             if index == 9:
                 pofp.append (
                     {'minutes': time_convert ( minute['dt'], timezone_offset ).strftime("%-I:%M %p"), 
                     'precipitation': percent/10}
                     )
-                print_time = time_convert ( minute['dt'], timezone_offset )
-                # print (f'{print_time.strftime("%-I:%M %p")} {percent/10}')
                 percent = 0
                 index = -1
             
@@ -77,16 +91,11 @@ def temp_graph (response):
             return ""
 
         df = pd.DataFrame(pofp)
-        first_time = df['minutes'].iloc[0]
-        last_time = df['minutes'].iloc[-1]
-        # print (f'{first_time} and {last_time}')
-
         fig = px.area(
             df, 
             x='minutes', 
             y='precipitation', 
             color_discrete_sequence = ['#578D7F'],
-            # title='percentage of precipitation', 
             )
         fig.update_layout(
             height=200,
@@ -101,24 +110,9 @@ def temp_graph (response):
             gridcolor='#A3C3BF',
             range = [0, 100],
             )
-    # https://plotly.com/python/reference/layout/yaxis/
     else:
         return ""
     return fig.to_html()
-
-'''
-  <div class="row">  <!-- Temp graph-->
-    <div class="col">
-      <div class="card border-1 bg-transparent">
-        <div class="card-body">
-          <div class="container">
-            {{ temp_graph | safe }}
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-'''
 
 def hourly_forcast (response):
     forcast = []
@@ -126,15 +120,36 @@ def hourly_forcast (response):
     if response.status_code == 200:
         data = response.json()
         timezone_offset = data['timezone_offset']
+        
+        previous_pressure = None  # To track the trend line
+        
         for hour in data['hourly']:
             hour_forcast = {}
-            hour_forcast['forcast_time'] =         time_convert ( hour['dt'], timezone_offset ).strftime("%-I:%M %p")
+            hour_forcast['forcast_time'] =       time_convert ( hour['dt'], timezone_offset ).strftime("%-I:%M %p")
             hour_forcast['temp'] =               float(round(hour['temp'],1))
             hour_forcast['feels_like'] =         float(round(hour['feels_like'],1))
-            hour_forcast['pressure'] =           hour['pressure']/10
+            
+            # OpenWeather sends pressure in hPa (e.g., 1013). You divide by 10 for kPa (101.3 kPa).
+            current_pressure = hour['pressure'] / 10
+            hour_forcast['pressure'] =           current_pressure
+            
+            # --- Calculate Pressure Trend ---
+            if previous_pressure is None:
+                hour_forcast['pressure_trend'] = "flat"  # First hour benchmark
+            elif current_pressure > previous_pressure:
+                hour_forcast['pressure_trend'] = "rising"
+            elif current_pressure < previous_pressure:
+                hour_forcast['pressure_trend'] = "falling"
+            else:
+                hour_forcast['pressure_trend'] = "flat"
+                
+            previous_pressure = current_pressure  # Update benchmark for the next hour block
+            # ---------------------------------
+
             hour_forcast['humidity'] =           hour['humidity']
             hour_forcast['clouds'] =             hour['clouds']
             hour_forcast['description'] =        hour['weather'][0]['description']
+            hour_forcast['icon'] =               hour['weather'][0]['icon']
             hour_forcast['wind_speed'] =         float(round(hour['wind_speed'],1))
             hour_forcast['wind_deg'] =           hour['wind_deg']
             hour_forcast['wind_direction'] =     degToCompass (hour['wind_deg'])
@@ -166,13 +181,17 @@ def daily_forcast (response):
             daily_forcast['clouds'] =             day['clouds']
             daily_forcast['pop'] =                int(day['pop']*100)
             daily_forcast['description'] =        day['weather'][0]['description']
+            daily_forcast['icon'] =               day['weather'][0]['icon']  # <-- Extracted daily icon code
             daily_forcast['wind_speed'] =         float(round(day['wind_speed'],1))
             daily_forcast['wind_deg'] =           day['wind_deg']
             daily_forcast['wind_direction'] =     degToCompass (day['wind_deg'])
+            phase_name, phase_icon = get_moon_phase_info(day['moon_phase'])
+            daily_forcast['moon_phase_name'] = phase_name
+            daily_forcast['moon_phase_icon'] = phase_icon
             try:
                 daily_forcast['wind_gust'] =      float(round(day['wind_gust'],1))
             except:
                 daily_forcast['wind_gust'] = 0
-            # print (daily_forcast)
             forcast.append(daily_forcast)
     return forcast
+
